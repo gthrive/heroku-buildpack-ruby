a2268e33ad3a4b525b321fe8c0c2053615f36bfe
Terence Lee <hone02@gmail.com>
2016-11-15 17:32:31 -0600

    staging bucket

--- lib/language_pack/base.rb
+++ lib/language_pack/base.rb
@@ -15,7 +15,7 @@ Encoding.default_external = Encoding::UTF_8 if defined?(Encoding)
 class LanguagePack::Base
   include LanguagePack::ShellHelpers
 
-  VENDOR_URL           = ENV['BUILDPACK_VENDOR_URL'] || "https://s3-external-1.amazonaws.com/heroku-buildpack-ruby"
+  VENDOR_URL           = ENV['BUILDPACK_VENDOR_URL'] || "https://s3-external-1.amazonaws.com/heroku-buildpack-ruby-staging"
   DEFAULT_LEGACY_STACK = "cedar"
   ROOT_DIR             = File.expand_path("../../..", __FILE__)
 
